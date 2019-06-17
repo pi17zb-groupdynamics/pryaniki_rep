b7f4a5033a7790ae49f677c1c54fbda5b8edbe63
AAZinoviev <alexander.alexeyevich.zinoviev@yandex.ru>
2019-06-17 21:49:48 +0300

    Init repo

---
+++ IPrimeCheckMethod.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    interface IPrimeCheckMethod
+    {
+        bool IsPrime(int number);
+    }
+}
