9c4c799848ffcdf51b12a7cd0bc409cfae7f05d4
Alexandra <maryjane4378@gmail.com>
2019-06-18 01:02:49 +0300

    Implemented IsPrime method

--- SieveOfEratosthenes.cs
+++ SieveOfEratosthenes.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace PrimeNumbers
 {
-    class SieveOfEratosthenes
+    class SieveOfEratosthenes : IPrimeCheckMethod
     {
+        public bool IsPrime(int number)
+        {
+            bool[] sieve = new bool[number + 1];
+
+            for (int i = 0; i < sieve.Length; i++)
+            {
+                sieve[i] = true;
+            }
+
+            for (int i = 2; i*i < sieve.Length; i++)
+            {
+                if (sieve[i])
+                {
+                    for (int j = i * 2; j < sieve.Length; j += i)
+                    {
+                        sieve[j] = false;
+                    }
+                }
+            }
+
+            if (sieve[number])
+                return true;
+
+            return false;
+        }
     }
 }
