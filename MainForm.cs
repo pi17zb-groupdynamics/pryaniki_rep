f4c84b56d4aaa1d0f1317b9eb4b102e734b4dad9
Евгений Бондаренко <bondarenk0.evgenii@yandex.ua>
2019-06-24 21:16:38 +0300

    Метод для определения простое число или не ,запускается в другом потоке

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PrimeNumbers
@@ -15,6 +16,7 @@ namespace PrimeNumbers
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Thread thread;
             if (radioButton1.Checked)
             {
                 method = new BustDivisors();
@@ -28,6 +30,14 @@ namespace PrimeNumbers
                 method = new SieveOfEratosthenes();
             }
 
+            thread = new Thread(implementation);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void implementation()
+        {
+            button1.Invoke((MethodInvoker)(() => button1.Enabled = false));
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
             if (numericUpDown1 == null)
@@ -35,9 +45,8 @@ namespace PrimeNumbers
 
             var isPrime = method.IsPrime((int)numericUpDown1.Value) ? "простое" : "не простое";
             watch.Stop();
-
-            label3.Text = $"Число {isPrime}, вычисление заняло {watch.Elapsed.TotalSeconds} секунд";
-            
+            label3.Invoke((MethodInvoker)(() => label3.Text = $"Число {isPrime}, вычисление заняло {Math.Round(watch.Elapsed.TotalSeconds, 6)} секунд"));
+            button1.Invoke((MethodInvoker)(() => button1.Enabled = true));
         }
     }
 }
